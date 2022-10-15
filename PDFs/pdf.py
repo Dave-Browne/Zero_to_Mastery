9ae1846b55e6fd578e93d140f98adb4e0e466dac
mheynike <56406325+mheynike@users.noreply.github.com>
2022-10-16 00:50:16 +0100

    Average solution for rotate, can do with more user help

--- PDFs/pdf.py
+++ PDFs/pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import PyPDF2
-#import sys
+import sys
 import os.path
 import argparse
 
@@ -10,16 +10,38 @@ import argparse
 #@click.argument('pdf_file', nargs=1, type=click.Path())
 #@click.argument('page', default=1, nargs=1)
 #@click.argument('num_rotations', default=1, nargs=1)
-def pdf_rotateCW_90(pdf_file, page=1, num_rotations=1):
+def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list=1):
   """
   Rotate a page 90deg clockwise in a .pdf file and save it to a new file
+  Rotate multiple pages by making a list, eg python pdf.py -r test.pdf [1,2,3] [2,2,1]
   """
+  # Process user input
+  disallowed_chars = '[]'
+  for ch in disallowed_chars:
+    pages_str_list = pages_str_list.replace(ch, "")
+    num_rotations_str_list = num_rotations_str_list.replace(ch, "")
+  pages_str_list = pages_str_list.split(",")
+  num_rotations_str_list = num_rotations_str_list.split(",")
+
+  pages = []
+  num_rotations = []
+
   try:
     assert pdf_file.split(".")[1] == 'pdf'
     assert os.path.isfile(pdf_file)
+    assert len(pages_str_list) >= len(num_rotations_str_list)
+    for i in pages_str_list:
+      pages.append(int(i) - 1)
+    for i in num_rotations_str_list:
+      num_rotations.append(int(i))
+    while len(pages) > len(num_rotations):
+      num_rotations.append(1)
+  except AssertionError:
+    print('Use <filename.pdf> [pages] [num_rotations] as arguments')
+    sys.exit()
   except:
-    print('Please use <filename.pdf> as an argument')
-#    sys.exit()
+    print('Page number and number of rotations must be integers')
+    sys.exit()
 
   file = open(pdf_file, 'rb')
   reader = PyPDF2.PdfReader(file)
@@ -27,8 +49,10 @@ def pdf_rotateCW_90(pdf_file, page=1, num_rotations=1):
 
   for page_num in range(len(reader.pages)):
     current_page = reader.pages[page_num]
-    if page_num == (page - 1):
-      for i in range(num_rotations):
+    if page_num in pages:
+      ind = pages.index(page_num)
+      num_rotate = num_rotations[ind]
+      for i in range(num_rotate):
         current_page.rotateClockwise(90)
     writer.add_page(current_page)
 
@@ -41,8 +65,6 @@ def pdf_rotateCW_90(pdf_file, page=1, num_rotations=1):
   new_file.close()
 
 
-#@cli.command(name='concatenate', help='args: concatenates 2+ .pdf files')
-#@click.argument('pdf_list', nargs=-1, type=click.Path())
 def pdf_concatenate(pdf_list):
   """
   Concatenates list of input .pdf files into a new concatenated .pdf file
@@ -52,9 +74,9 @@ def pdf_concatenate(pdf_list):
     for file in pdf_list:
       assert file.split(".")[1] == 'pdf'
       assert os.path.isfile(file)
-  except:
-    print('Please use 2+ <filename.pdf> files as an argument')
-#    sys.exit()
+  except AssertionError:
+    print('Use 2+ <filename.pdf> files as arguments')
+    sys.exit()
 
   merger = PyPDF2.PdfMerger()
   for file in pdf_list:
@@ -86,8 +108,8 @@ def pdf_merge(pdf_tuples):
       assert os.path.isfile(tuple[1])
       print('6')
   except:
-    print('Please use tuples: (position to insert [0:], <filename.pdf>, (start, stop[, step])) as arguments')
-#    sys.exit()
+    print('Use tuples: (position to insert [0:], <filename.pdf>, (start, stop[, step])) as arguments')
+    sys.exit()
 
   merger = PyPDF2.PdfMerger()
   for tuple in pdf_tuples:
@@ -100,9 +122,6 @@ def pdf_merge(pdf_tuples):
   merger.close()
 
 
-#@cli.command(name='delete', help='args: .pdf file, pages separated by a space')
-#@click.argument('pdf_file', nargs=1, type=click.Path())
-#@click.argument('pages_str', nargs=-1)
 def pdf_delete(pdf_file, pages_str):
   """
   Delete pages from a .pdf file and save it to a new file
@@ -115,10 +134,11 @@ def pdf_delete(pdf_file, pages_str):
     for i in pages_str:
       pages.append(int(i) - 1)
   except AssertionError:
-    print('Please use <filename.pdf> as an argument')
+    print('Use <filename.pdf> as an argument')
+    sys.exit()
   except:
     print('Pages to be deleted must be integers')
-#    sys.exit()
+    sys.exit()
 
   file = open(pdf_file, 'rb')
   reader = PyPDF2.PdfReader(file)
@@ -144,17 +164,15 @@ if __name__ == '__main__':
   group.add_argument('-c', '--concatenate', nargs='+', metavar='FILENAME.PDF')
   group.add_argument('-d', '--delete', nargs='+', metavar=('FILENAME.PDF', 'PAGE'))
   group.add_argument('-m', '--merge', nargs=4, action='append', metavar=('PG_INSERT', 'FILENAME.PDF', 'PG_START', 'PG_END'))
-  group.add_argument('-r', '--rotate', nargs=3, metavar=('FILENAME.PDF', 'PAGE', 'NUM_ROTATIONS_CW_90DEG'))
+  group.add_argument('-r', '--rotate', nargs=3, metavar=('FILENAME.PDF', '[PAGES]', '[NUM_ROTATIONS_CW_90DEG]'))
 
   args = parser.parse_args()
 
   if args.concatenate:
-    print(f'1 {args.concatenate}')      # 1 ['wtr.pdf', 'dummy.pdf']
     pdf_concatenate(args.concatenate)
-  if args.delete:
-    print(f'2 {args.delete}')           # 2 ['wtr.pdf', '2', '5', '7']
-    pdf_delete()
-  if args.merge:
+  elif args.delete:
+    pdf_delete(args.delete[0], args.delete[1:])
+  elif args.merge:
     print(f'3 {args.merge}')            # 3 [['0', 'wtr.pdf', '0', '0'], ['1', 'twopage.pdf', '0', '1']]
-  if args.rotate:
-    print(f'4 {args.rotate}')           # 4 ['wtr.pdf', '1', '2']
+  elif args.rotate:
+    pdf_rotateCW_90(args.rotate[0], args.rotate[1], args.rotate[2])
