2a52c7bd52b417d596848d7de633838565df6e9b
David Browne <davidmgbrowne@gmail.com>
2022-10-18 00:28:06 +0100

    Add files via upload
    Tested all options are now working.

--- PDFs/pdf.py
+++ PDFs/pdf.py
@@ -12,16 +12,17 @@ def pdf_concatenate(pdf_list):
   """
   try:
     assert len(pdf_list) > 1
-    for file in pdf_list:
-      assert file.split(".")[1] == 'pdf'
-      assert os.path.isfile(file)
+    for pdf_file in pdf_list:
+      assert '.' in pdf_file
+      assert pdf_file.split(".")[1] == 'pdf'
+      assert os.path.isfile(pdf_file)
   except AssertionError:
     print('Use 2+ <filename.pdf> files as arguments')
     sys.exit()
 
   merger = PyPDF2.PdfMerger()
-  for file in pdf_list:
-    merger.append(file)
+  for pdf_file in pdf_list:
+    merger.append(pdf_file)
 
   filename = 'concatenated.pdf'
   merger.write(filename)
@@ -36,6 +37,7 @@ def pdf_delete(pdf_file, pages_str):
   pages = []
 
   try:
+    assert '.' in pdf_file
     assert pdf_file.split(".")[1] == 'pdf'
     assert os.path.isfile(pdf_file)
     for i in pages_str:
@@ -64,16 +66,18 @@ def pdf_delete(pdf_file, pages_str):
   file.close()
   new_file.close()  
   
-def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list=1):
+def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list):
   """
   Rotate a page 90deg clockwise in a .pdf file and save it to a new file
-  Rotate multiple pages by making a list, eg python pdf.py -r test.pdf [1,2,3] [2,2,1]
-  args: ['test.pdf', '[1,2,3]', '[2,2,1]']
+  Rotate multiple pages by making a list, eg python pdf.py -r test.pdf "1,2,3" "2,2,1"
+  args: ['test.pdf', '1,2,3', '2,2,1']
   """
   # Process user input
   pages = []
   num_rotations = []
   disallowed_chars = '[] '
+
+  print(pages_str_list)
 
   try:
     for ch in disallowed_chars:
@@ -81,6 +85,7 @@ def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list=1):
       num_rotations_str_list = num_rotations_str_list.replace(ch, "")
     pages_str_list = pages_str_list.split(",")
     num_rotations_str_list = num_rotations_str_list.split(",")
+    assert '.' in pdf_file
     assert pdf_file.split(".")[1] == 'pdf'
     assert os.path.isfile(pdf_file)
     assert len(pages_str_list) >= len(num_rotations_str_list)
@@ -91,7 +96,7 @@ def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list=1):
     while len(pages) > len(num_rotations):
       num_rotations.append(1)
   except AssertionError:
-    print('Use <filename.pdf> [pages] [num_rotations] as arguments')
+    print('Use <filename.pdf> "pages" "num_rotations" as arguments')
     sys.exit()
   except:
     print('Page number and number of rotations must be integers')
@@ -121,31 +126,31 @@ def pdf_rotateCW_90(pdf_file, pages_str_list, num_rotations_str_list=1):
 def pdf_merge(pdf_input_data):
   """
   Weld pdf files together and save it to a new file
-  eg python pdf.py -m [a.pdf, 1, 5] [b.pdf, 1, 3] [a.pdf, 6, 10] [c.pdf, 2, 3]
-  args: ['[a.pdf, 1, 5]', '[b.pdf, 1, 3]', '[a.pdf, 6, 10]', '[c.pdf, 2, 3]']
+  eg python pdf.py -m "a.pdf, 1, 5" "b.pdf, 1, 3" "a.pdf, 6, 10" "c.pdf, 2, 3"
+  args: ['a.pdf 1 5', 'b.pdf 1 3', 'a.pdf 6 10', 'c.pdf 2 3']
   """
   # Process user input
   pdf_data = []
   data = []
-  disallowed_chars = '[] '
+  disallowed_chars = '[]'
+  counter = 0
 
-  print(f'pdf_input_data {pdf_input_data}')
   try:
     for input_data in pdf_input_data:
       for ch in disallowed_chars:
-        input_data = input_data.replace(ch, "")                   # ['a.pdf,1,5']
-      print(f'input_data_nochar {input_data}')
-      input_data = input_data.split(",")                          # ['a.pdf','1','5']
-      print(f'input_data_split  {input_data}')
+        input_data = input_data.replace(ch, "")
+      input_data = input_data.split(" ")                                                    # ['a.pdf', '1', '5']
 
-      # Temporarily use 0 as a placemarker PdfMerger insert page
-      pdf_data.append([0, input_data[0], int(input_data[1])-1, int(input_data[2])-1])       # [[0,'a.pdf',0,4], [0,'b.pdf',0,2], [0,'a.pdf',5,9], [0,'c.pdf',1,2]]
-      print(f'data_corrected--- {pdf_data}')
+      # Temporarily use 0 as a placemarker for PdfMerger insert page argument
+      # Stop value is not included in merger so don't deduct 1
+      pdf_data.append([0, input_data[0], int(input_data[1])-1, int(input_data[2])])         # [[0,'a.pdf',0,5], [0,'b.pdf',0,3], [0,'a.pdf',5,10], [0,'c.pdf',1,3]]
     
-      assert pdf_data[1].split(".")[1] == 'pdf'
-      assert os.path.isfile(pdf_data[1])
+      assert '.' in pdf_data[counter][1]
+      assert pdf_data[counter][1].split(".")[1] == 'pdf'
+      assert os.path.isfile(pdf_data[counter][1])
+      counter += 1
   except:
-    print('Use [<filename.pdf> pg_start pg_stop] [<filename.pdf> pg_start pg_stop] as arguments')
+    print('Use at least 2 args like "<filename.pdf> pg_start pg_stop" "<filename.pdf> pg_start pg_stop"')
     sys.exit()
 
   merger = PyPDF2.PdfMerger()
@@ -153,16 +158,13 @@ def pdf_merge(pdf_input_data):
   # Calculate the insert page pdf_data[1:][0] for the PdfMerger
   for i in range(1, len(pdf_data)):
     pdf_data[i][0] = pdf_data[i-1][0] + pdf_data[i-1][3] - pdf_data[i-1][2] + 1             # [[0,'a.pdf',0,4], [5,'b.pdf',0,2], [8,'a.pdf',5,9], [13,'c.pdf',1,2]]
-  print(f'pdf_data_insert_pos {pdf_data}')
       
   # Create a tuple of (start, stop) page for the PdfMerger
   for i in range(len(pdf_data)):
-    pdf_data[i][2] = tuple(pdf_data[i][2], pdf_data[i][3])
-    del pdf_data[i][3]                                            # [[0,'a.pdf',(0,4)], [5,'b.pdf',(0,2)], [8,'a.pdf',(5,9)], [13,'c.pdf',(1,2)]]
+    pdf_data[i][2] = (pdf_data[i][2], pdf_data[i][3])
+    del pdf_data[i][3]                                                                      # [[0,'a.pdf',(0,4)], [5,'b.pdf',(0,2)], [8,'a.pdf',(5,9)], [13,'c.pdf',(1,2)]]
     
     merger.merge(position=pdf_data[i][0], fileobj=pdf_data[i][1], pages=pdf_data[i][2])
-
-  print(f'pdf_data_tuple {pdf_data}')
 
   filename = 'merged.pdf'
   merger.write(filename)
@@ -174,18 +176,18 @@ def pdf_merge(pdf_input_data):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Manipulate .pdf files")
   group = parser.add_mutually_exclusive_group()
-  group.add_argument('-c', '--concatenate', nargs='+', metavar='FILENAME.PDF')
-  group.add_argument('-d', '--delete', nargs='+', metavar=('FILENAME.PDF', 'PAGE'))
-  group.add_argument('-r', '--rotate', nargs=3, metavar=('FILENAME.PDF', '[PAGES]', '[NUM_ROTATIONS_CW_90DEG]'), help = 'eg python pdf.py -r test.pdf [1,2,3] [2,2,1]')
-  group.add_argument('-m', '--merge', nargs=4, action='append', metavar=('PG_INSERT', 'FILENAME.PDF', 'PG_START', 'PG_STOP', help='python pdf.py -m [a.pdf, 1, 5] [b.pdf, 1, 3] [a.pdf, 6, 10] [c.pdf, 2, 3]'))
+  group.add_argument('-c', nargs='+', metavar='FILENAME.PDF', help = 'CONCATENATE eg python pdf.py -c a.pdf b.pdf c.pdf')
+  group.add_argument('-d', nargs='+', metavar=('FILENAME.PDF', 'PAGE'), help = 'DELETE eg python pdf.py -d a.pdf 2 3 5')
+  group.add_argument('-r', nargs=3, metavar=('FILENAME.PDF', '[PAGES]', '[NUM_ROTATIONS_CW_90DEG]'), help = 'ROTATE eg python pdf.py -r test.pdf "1,2,3" "2,2,1"')
+  group.add_argument('-m', nargs=3, action='append', metavar=('FILENAME.PDF', 'PG_START', 'PG_STOP'), help = 'MERGE eg python pdf.py -m "a.pdf, 1, 5" "b.pdf, 1, 3" "a.pdf, 6, 10" "c.pdf, 2, 3"')
 
   args = parser.parse_args()
 
-  if args.concatenate:
-    pdf_concatenate(args.concatenate)
-  elif args.delete:
-    pdf_delete(args.delete[0], args.delete[1:])
-  elif args.rotate:
-    pdf_rotateCW_90(args.rotate[0], args.rotate[1], args.rotate[2])
-  elif args.merge:
-    pdf_merge(args.merge)
+  if args.c:
+    pdf_concatenate(args.c)
+  elif args.d:
+    pdf_delete(args.d[0], args.d[1:])
+  elif args.r:
+    pdf_rotateCW_90(args.r[0], args.r[1], args.r[2])
+  elif args.m:
+    pdf_merge(args.m[0])
